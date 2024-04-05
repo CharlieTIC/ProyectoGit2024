6c40b32458bb9b1a0790fa2f8e50cc1eec8deb2c
CharlieTIC <carlos.ffptg@gmail.com>
2024-04-05 10:01:01 +0200

    Version 1.0

---
+++ src/testpower/Aplicacion.java
@@ -0,0 +1,30 @@
+package testpower;
+
+public class Aplicacion {
+
+	public static void main(String[] args) {
+	
+		System.out.println("-------------------------------");
+		System.out.println("Aplicación de test de memoria");
+		System.out.println("-------------------------------");
+		System.out.println("-------------------------------");
+		System.out.println("---------------3---------------");
+		System.out.println("---------------2---------------");
+		System.out.println("---------------1---------------");
+		
+		
+		for (int i = 0; i <= 100000; i++) {
+			
+			System.out.println("Calentando motores");
+			
+		}
+		
+		
+		System.out.println("La prueba a finalizado");
+		
+	
+	}
+
+}
+
+
